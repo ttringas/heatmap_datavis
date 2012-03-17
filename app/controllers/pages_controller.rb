f2bdaf68cd0a705441082c22154eb3789b5c40d2
Tyler Tringas <ttringas@gmail.com>
2012-03-17 16:07:04 -0400

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def index
+  end
+  def counties
+  end
+  def data
+  end
+  def states
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heatmap::Application.config.secret_token = 'c9cff60df4f68edea2f2125bc73979c7a54155615cc1a56270b985653531d3dd75ae944de09304d24567cabb1e2d1e8a1159536bb68851bbdaf5374b2208838c'
